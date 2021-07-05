f4b6cdd217042c1cca5236471440a1567b7ebadc
Afif Zuhair <afifzuhair08@gmail.com>
2021-07-05 23:37:06 +0800

    Level 2 - encryption using mongoose-encrypt, env variables & gitignore

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.set("view engine","ejs");
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
-// schema
-const userSchema = {
+// SCHEMA
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-};
+});
 
-// model
+// ENCRYPTION & PLUGIN
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
+
+// MODEL
 const User = new mongoose.model("User", userSchema);
 
 // MAIN ROUTE
@@ -66,8 +70,8 @@ app.post("/login", function(req, res){
                 }
             }
         }
-    })
-})
+    });
+});
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
